c947c8a806056735927490ab04b7226605a3dbce
tharinDmahale <deelaka.mahale@gmail.com>
2025-12-10 16:35:37 +0530

    project init

---
+++ clearbasket.php
@@ -0,0 +1,27 @@
+<?php
+
+session_start();
+include ("db.php");
+
+
+$pagename="Clear Smart Basket";
+echo "<link rel=stylesheet type=text/css href=mystylesheet.css>";
+
+echo "<title>".$pagename."</title>";
+
+echo "<body>";
+
+include ("headfile.html");
+include("detectlogin.php");
+
+echo "<h4>".$pagename."</h4>";
+
+unset($_SESSION['basket']);
+
+echo "<p>Your basket was cleared!</p>";
+echo "<p>You may go back to the <a href='basket.php'>basket</a></p>";
+
+include("footfile.html");
+
+echo "</body>";
+?>
